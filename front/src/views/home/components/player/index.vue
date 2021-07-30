d2f4b0853aac724a8a96748ab824fb60f61655c6
钟辉 <hui.zhong@wholion.com>
2021-07-30 11:37:10 +0800

    feat: 为播放器添加进度条

--- front/src/views/home/components/player/index.vue
+++ front/src/views/home/components/player/index.vue
@@ -1,91 +1,116 @@
 <template>
   <!-- 底部播放器 -->
-  <div class="music-player">
-    <audio
-      ref="audioPlayerRef"
-      class="audio-element"
-      :src="currentSongUrl"
-      controls
-    ></audio>
-    <!-- 播放器左侧 -->
-    <div class="left-part">
-      <div v-if="playingSong" class="wrapper">
-        <div class="cover-img">
-          <img :src="playingSong.coverImg" alt="歌曲封面图片" />
-        </div>
-        <div class="song-info-wrapper">
-          <div class="song-info">
-            <span class="song-name word-ellipsis">{{ playingSong.name }}</span>
-            <div class="parting-line">-</div>
-            <span class="artist word-ellipsis">{{ playingSongArtistStr }}</span>
-          </div>
-          <div class="play-pace">
-            <span class="time">
-              {{ transformSecondToMinute(songState.playedSongDuration) }}
-            </span>
-            <span class="parting-line">/</span>
-            <span class="time">
-              {{ transformSecondToMinute(songState.songDuration) }}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- 播放器中部 -->
-    <div class="center-part">
-      <div class="prev-song-wrapper" @click="playPrev">
-        <mp-icon icon="prev-song" color="#d33a30" :size="16" />
-      </div>
-      <div v-if="songState.isPause" class="play-song-wrapper" @click="playSong">
-        <mp-icon icon="play-button" color="#d33a30" :size="40" :scale="1" />
-      </div>
-      <div v-else class="play-song-wrapper" @click="playPause">
-        <mp-icon icon="pause-button" color="#d33a30" :size="40" :scale="1" />
-      </div>
-      <div class="next-song-wrapper" @click="playNext">
-        <mp-icon icon="next-song" color="#d33a30" :size="16" />
-      </div>
-    </div>
-    <!-- 播放器右侧 -->
-    <div class="right-part">
+  <div class="music-player-wrapper">
+    <div class="music-player">
       <div class="wrapper">
-        <div
-          class="play-list-wrapper mr-20"
-          @click="changeListState(playerState.listState)"
-        >
-          <el-tooltip
-            placement="top"
-            :content="playerState.listStateDesc"
-            :visible-arrow="false"
-            effect="light"
+        <audio
+          ref="audioPlayerRef"
+          class="audio-element"
+          :src="currentSongUrl"
+          controls
+        ></audio>
+        <!-- 播放器左侧 -->
+        <div class="left-part">
+          <div v-if="playingSong" class="wrapper">
+            <div class="cover-img">
+              <img :src="playingSong.coverImg" alt="歌曲封面图片" />
+            </div>
+            <div class="song-info-wrapper">
+              <div class="song-info">
+                <span class="song-name word-ellipsis">{{
+                  playingSong.name
+                }}</span>
+                <div class="parting-line">-</div>
+                <span class="artist word-ellipsis">{{
+                  playingSongArtistStr
+                }}</span>
+              </div>
+              <div class="play-pace">
+                <span class="time">
+                  {{ transformSecondToMinute(songState.playedSongDuration) }}
+                </span>
+                <span class="parting-line">/</span>
+                <span class="time">
+                  {{ transformSecondToMinute(songState.songDuration) }}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 播放器中部 -->
+        <div class="center-part">
+          <div class="prev-song-wrapper" @click="playPrev">
+            <mp-icon icon="prev-song" color="#d33a30" :size="16" />
+          </div>
+          <div
+            v-if="songState.isPause"
+            class="play-song-wrapper"
+            @click="playSong"
           >
+            <mp-icon icon="play-button" color="#d33a30" :size="40" :scale="1" />
+          </div>
+          <div v-else class="play-song-wrapper" @click="playPause">
             <mp-icon
-              :icon="playerState.listStateIcon"
-              color="#4b4b4b"
-              :size="16"
+              icon="pause-button"
+              color="#d33a30"
+              :size="40"
               :scale="1"
             />
-          </el-tooltip>
+          </div>
+          <div class="next-song-wrapper" @click="playNext">
+            <mp-icon icon="next-song" color="#d33a30" :size="16" />
+          </div>
         </div>
-        <div class="play-list-wrapper mr-20" @click.stop="toggleExpandSong()">
-          <mp-icon
-            icon="play-list"
-            :color="playerState.expandSong ? '#d33a30' : '#4b4b4b'"
-            :size="16"
-            :scale="1"
-          />
-        </div>
-        <div class="show-word-wrapper mr-20">
-          <mp-icon icon="word" color="#4b4b4b" :size="16" :scale="1" />
-        </div>
-        <volume-adjuster @change-volume="onChangeVolume">
-          <template v-slot:content>
-            <div class="adjust-volume-wrapper mr-20">
-              <mp-icon icon="horn" color="#4b4b4b" :size="16" :scale="1" />
+        <!-- 播放器右侧 -->
+        <div class="right-part">
+          <div class="wrapper">
+            <div
+              class="play-list-wrapper mr-20"
+              @click="changeListState(playerState.listState)"
+            >
+              <el-tooltip
+                placement="top"
+                :content="playerState.listStateDesc"
+                :visible-arrow="false"
+                effect="light"
+              >
+                <mp-icon
+                  :icon="playerState.listStateIcon"
+                  color="#4b4b4b"
+                  :size="16"
+                  :scale="1"
+                />
+              </el-tooltip>
             </div>
-          </template>
-        </volume-adjuster>
+            <div
+              class="play-list-wrapper mr-20"
+              @click.stop="toggleExpandSong()"
+            >
+              <mp-icon
+                icon="play-list"
+                :color="playerState.expandSong ? '#d33a30' : '#4b4b4b'"
+                :size="16"
+                :scale="1"
+              />
+            </div>
+            <div class="show-word-wrapper mr-20">
+              <mp-icon icon="word" color="#4b4b4b" :size="16" :scale="1" />
+            </div>
+            <volume-adjuster @change-volume="onChangeVolume">
+              <template v-slot:content>
+                <div class="adjust-volume-wrapper mr-20">
+                  <mp-icon icon="horn" color="#4b4b4b" :size="16" :scale="1" />
+                </div>
+              </template>
+            </volume-adjuster>
+          </div>
+        </div>
       </div>
+      <!-- 歌曲进度条 -->
+      <div
+        class="progress-bar"
+        :style="{ width: `${songState.playRate * 100}%` }"
+      ></div>
     </div>
   </div>
   <!-- 右侧播放列表 -->
@@ -391,7 +416,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.music-player {
+.music-player-wrapper {
   position: absolute;
   left: 0;
   bottom: 0;
@@ -404,105 +429,127 @@ export default defineComponent({
   height: 60px;
   box-sizing: border-box;
   background-color: #fff;
-  border-top: 2px solid #f5f5f5;
 
-  .left-part {
-    flex: 1;
+  .music-player {
     display: flex;
     align-items: center;
-    padding: 10px;
-    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    border-top: 2px solid #f5f5f5;
+    position: relative;
 
     & > .wrapper {
       display: flex;
       align-items: center;
-
-      .cover-img {
-        width: 40px;
-        height: 40px;
-        border-radius: 4px;
-        margin-right: 10px;
-
-        & > img {
-          width: 100%;
-          height: 100%;
-          border-radius: 4px;
-        }
-      }
-
-      .song-info-wrapper {
+      width: 100%;
+      height: 100%;
+      .left-part {
+        flex: 1;
         display: flex;
-        flex-direction: column;
-        height: 40px;
-        justify-content: center;
+        align-items: center;
+        padding: 10px;
+        box-sizing: border-box;
 
-        .song-info {
+        & > .wrapper {
           display: flex;
           align-items: center;
-          margin-bottom: 8px;
-          .song-name {
-            max-width: 200px;
-            color: #4d4d4d;
-            font-size: 14px;
-            font-weight: bold;
+
+          .cover-img {
+            width: 40px;
+            height: 40px;
+            border-radius: 4px;
+            margin-right: 10px;
+
+            & > img {
+              width: 100%;
+              height: 100%;
+              border-radius: 4px;
+            }
           }
-          .parting-line {
-            color: #666666;
-            font-size: 13px;
-            padding: 0 4px;
-          }
-          .artist {
-            max-width: 150px;
-            color: #666666;
-            font-size: 13px;
-          }
-        }
-        .play-pace {
-          display: flex;
-          align-items: center;
-          .time {
-            color: #bbbbbb;
-            font-size: 12px;
-          }
-          .parting-line {
-            color: #bbbbbb;
-            font-size: 12px;
-            padding: 0 2px;
+
+          .song-info-wrapper {
+            display: flex;
+            flex-direction: column;
+            height: 40px;
+            justify-content: center;
+
+            .song-info {
+              display: flex;
+              align-items: center;
+              margin-bottom: 8px;
+              .song-name {
+                max-width: 200px;
+                color: #4d4d4d;
+                font-size: 14px;
+                font-weight: bold;
+              }
+              .parting-line {
+                color: #666666;
+                font-size: 13px;
+                padding: 0 4px;
+              }
+              .artist {
+                max-width: 150px;
+                color: #666666;
+                font-size: 13px;
+              }
+            }
+            .play-pace {
+              display: flex;
+              align-items: center;
+              .time {
+                color: #bbbbbb;
+                font-size: 12px;
+              }
+              .parting-line {
+                color: #bbbbbb;
+                font-size: 12px;
+                padding: 0 2px;
+              }
+            }
           }
         }
       }
-    }
-  }
 
-  .center-part {
-    display: flex;
-    align-items: center;
-    .prev-song-wrapper {
-      margin-left: 36px;
-      margin-right: 24px;
-    }
-    .next-song-wrapper {
-      margin-left: 24px;
-      margin-right: 36px;
-    }
-  }
+      .center-part {
+        display: flex;
+        align-items: center;
+        .prev-song-wrapper {
+          margin-left: 36px;
+          margin-right: 24px;
+        }
+        .next-song-wrapper {
+          margin-left: 24px;
+          margin-right: 36px;
+        }
+      }
 
-  .right-part {
-    flex: 1;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+      .right-part {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
 
-    .wrapper {
-      flex: 1;
-      align-self: flex-end;
-      display: flex;
-      align-items: center;
+        .wrapper {
+          flex: 1;
+          align-self: flex-end;
+          display: flex;
+          align-items: center;
+        }
+      }
+
+      .audio-element {
+        display: none;
+      }
     }
-  }
 
-  .audio-element {
-    display: none;
+    .progress-bar {
+      position: absolute;
+      left: 0;
+      top: -2px;
+      height: 2px;
+      background-color: #d33a30;
+    }
   }
 
   :deep(.mr-20) {
