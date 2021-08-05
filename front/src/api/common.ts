792476db480bef22b8a11733e6998fd4bca618e0
zhonghui <1126765590@qq.com>
2021-08-05 21:57:02 +0800

    chore: 修改服务端接口请求地址

--- front/src/api/common.ts
+++ front/src/api/common.ts
@@ -1,14 +1,14 @@
 const serverUrl =
   process.env.NODE_ENV === "production"
-    ? "https://music-player.immortalboy.cn/player"
+    ? "https://music-player.immortalboy.cn"
     : "";
 const imgServerUrl =
   process.env.NODE_ENV === "production"
-    ? "https://music-player.immortalboy.cn/player"
+    ? "https://music-player.immortalboy.cn"
     : "";
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://music-player.immortalboy.cn/player"
+    ? "https://music-player.immortalboy.cn"
     : "";
 
 export { baseUrl, serverUrl, imgServerUrl };
