18cfa2d33880e16a98475dd806e45a0f3af37efa
钟辉 <hui.zhong@wholion.com>
2021-08-12 17:58:36 +0800

    feat: 新增歌手详情

---
+++ front/src/views/home/components/main-content/views/artist-detail/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="artist-detail">
+    <!-- 歌手头像 + 名字 -->
+    <div class="brief-info"></div>
+    <!-- 专辑 + MV + 详情 + 相似歌手 -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ArtistDetail",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
