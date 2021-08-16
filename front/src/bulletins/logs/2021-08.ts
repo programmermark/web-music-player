5ab1e9092d27bbe7a7915e18b658f68990b7b37e
钟辉 <hui.zhong@wholion.com>
2021-08-16 10:25:58 +0800

    chore: 更新公告日志

--- front/src/bulletins/logs/2021-08.ts
+++ front/src/bulletins/logs/2021-08.ts
@@ -2,6 +2,12 @@ import { ILog } from "@/store/modules/interface/bulletin";
 
 export const logs: ILog[] = [
   {
+    date: "2021-08-16",
+    title: "web-music-player更新公告(2021-08-16)",
+    briefContent: `<p>1. 新增页面最新音乐-新歌速递；</p>`,
+    content: `<p>1. 新增页面最新音乐-新歌速递；</p>`,
+  },
+  {
     date: "2021-08-13",
     title: "web-music-player更新公告(2021-08-13)",
     briefContent: `<p>1. 新增全局的更新公告弹窗组件，显示简略公告信息；</p>
