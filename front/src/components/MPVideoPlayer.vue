a58e4443595981d5d65fa893ecfb72bdfce2e7dc
zhonghui <1126765590@qq.com>
2021-09-06 09:02:51 +0800

    feat: 新增MV详情代码

---
+++ front/src/components/MPVideoPlayer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mp-video-player">
+    <video class="player" :width="width" :height="height">
+      <source :src="src" />
+    </video>
+  </div>
+</template>
+
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    src: string /** 视频资源地址 */;
+    width: number /** 视频播放器的宽度 */;
+    height: number /** 视频播放器的高度 */;
+  }>(),
+  {
+    width: 640,
+    height: 360,
+  }
+);
+</script>
+
+<style lang="scss" scoped>
+.mp-video-player {
+  .player {
+    border-radius: 8px;
+  }
+}
+</style>
