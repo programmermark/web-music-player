d44db3f02e677dd2ea75117c996e84b658fcc896
zhonghui <1126765590@qq.com>
2021-09-13 20:34:46 +0800

    chore: 修改front-vite路由配置文件，动态导入的路径

--- front-vite/src/common/js/util/index.ts
+++ front-vite/src/common/js/util/index.ts
@@ -6,7 +6,8 @@ import { IDateFormat } from "./interface";
  * @param {string} viewPath  view的相对路径，相对于src目录
  */
 export function lazyLoad(viewPath: string) {
-  return () => import(/* @vite-ignore */ `/src/views/${viewPath}.vue`);
+  const prefix = import.meta.env.BASE_URL;
+  return () => import(/* @vite-ignore */ `../views/${viewPath}.vue`);
 }
 
 /** 返回指定类型的对象的键名数组 */
--- front-vite/src/router/index.ts
+++ front-vite/src/router/index.ts
@@ -4,7 +4,14 @@ import {
   createWebHistory,
   RouteRecordRaw,
 } from "vue-router";
-import { lazyLoad } from "@/common/js/util";
+
+/**
+ * 实现路由懒加载的工具函数
+ * @param {string} viewPath  view的相对路径，相对于src目录
+ */
+export function lazyLoad(viewPath: string) {
+  return () => import(/* @vite-ignore */ `../views/${viewPath}.vue`);
+}
 
 const routes: Array<RouteRecordRaw> = [
   {
