779cc4ea83778a9e10fb61b9d44bf07067ae15e8
zhonghui <1126765590@qq.com>
2021-09-24 00:39:13 +0800

    feat: 新增基础组件MPSearch

---
+++ front/src/components/MPSearch.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="inline-flex items-center w-44 h-6 bg-gray-100 rounded-xl">
+    <MPOptIcon
+      class="ml-2"
+      icon="search"
+      :size="13"
+      scale="1"
+      color="#555555"
+      bgColor="none"
+      display="always"
+    />
+    <input
+      class="
+        flex-1
+        w-28
+        h-4
+        bg-gray-100
+        ml-1
+        mr-2
+        outline-none
+        text-xs text-gray-600
+      "
+      :value="modelValue"
+      @input="handleInputChange"
+      type="text"
+      placeholder="搜索"
+    />
+    <MPOptIcon
+      v-show="clearable && modelValue"
+      class="mr-2"
+      icon="close-thin"
+      :size="13"
+      scale="1"
+      color="#555555"
+      bgColor="none"
+      display="always"
+      @click="handleClear"
+    />
+  </div>
+</template>
+
+<!-- 基础的搜索组件 -->
+<script lang="ts" setup>
+import MPOptIcon from "./MPOptIcon.vue";
+
+withDefaults(
+  defineProps<{
+    modelValue?: string /** 搜索框的输入值 */;
+    clearable?: boolean /** 是否开启清空功能 */;
+  }>(),
+  {
+    modelValue: "",
+    clearable: true,
+  }
+);
+
+const emits = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+
+const handleInputChange = (e: Event) => {
+  emits("update:modelValue", (e.target as HTMLInputElement).value);
+};
+
+/** 清空输入框 */
+const handleClear = () => {
+  emits("update:modelValue", "");
+};
+</script>
+
+<style lang="scss" scoped></style>
